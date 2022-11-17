054f82ac57d4817c6ca589e34946640897b8f41c
aymironov <aymironov@domclick.ru>
2022-11-17 10:26:03 +0300

    lsm2: fix lsm init dataclass

--- lsm_dev/lsm.py
+++ lsm_dev/lsm.py
@@ -4,46 +4,76 @@ import yaml
 
 import serial
 
+from port import Port
+
 #from asyncio.timeouts import timeout
 
+class ValData(BaseModel):
+    type: str
+    values: list[str]
 
+class ValuesData(BaseModel):
+    values: list[ValData]
 
+class Query(BaseModel):
+    command: str
+    address: bool
+    data: ValuesData
+    crc: bool
 
-        
-class SimpleCom(BaseModel):
+class Command(BaseModel):
     name: str
     desc: str
-    query: str
-    response: str 
+    request: Query
+    response: Query
+    timeout: int
 
-class ValuesStepListParam(BaseModel):
+class Commands(BaseModel):
+    commands: list[Command]
+
+
+
+class ValuesStepListSetting(BaseModel):
     start: float
     end: float
     step: float
 
-class KeysStepListParam(BaseModel):
+class KeysStepListSetting(BaseModel):
     start: float
     end: float
     step: float
     name: str
 
-class SimpleCommands(BaseModel):
-    commands: list[SimpleCom]
-
-class ListParam(BaseModel):
+class ListSetting(BaseModel):
     keys: list[str]
     values: list[int]
 
-class StepListParam(BaseModel):
-    keys: KeysStepListParam
-    values: ValuesStepListParam
+class StepListSetting(BaseModel):
+    keys: KeysStepListSetting
+    values: ValuesStepListSetting
+
+class SettingModem(BaseModel):
+    name: str
+    type: str
+    param: Union[ListSetting, StepListSetting]
+
+class ParamValue(BaseModel):
+    name: str
+    mask: int
+    shift: int
+    mul: int
+
+class ParamsValues(BaseModel):
+    name: str
+    type: str
+    params: list[ParamValue]
 
 class Params(BaseModel):
     name: str
     type: str
-    param: Union[ListParam, StepListParam]
-
-class BaseParams(BaseModel):
+    values: list[ParamsValues]
+class LSMParams(BaseModel):
+    setting_modem: list[SettingModem]
     params: list[Params]
 
 class Address(BaseModel):
@@ -57,18 +87,17 @@ class Addresses(BaseModel):
 class LSM:
     def __init__(self):
         self.port = Port()
-        with open(".\config\simple_command.yml", 'r',  encoding='utf8') as file_yml:
+        with open(".\config\command.yml", 'r',  encoding='utf8') as file_yml:
             command_ = yaml.safe_load(file_yml)
-            
-        self.commands = SimpleCommands(**command_)
+        self.commands = Commands(**command_)
 
         with open(".\config\params.yml", 'r',  encoding='utf8') as file_yml:
             params_ = yaml.safe_load(file_yml)
             print(params_)
-        self.params = BaseParams(**params_)
+        self.params = LSMParams(**params_)
 
-        with open(".\config\\address.yml", 'r',  encoding='utf8') as file_yml:
+        with open(".\config\address.yml", 'r',  encoding='utf8') as file_yml:
             address_ = yaml.safe_load(file_yml)
             print(address_)
-        #self.address = Address(**address_)
+        self.address = Address(**address_)
 
