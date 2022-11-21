de27a532b8bd3a718f53d50892e26a8748be3cfc
aymironov <aymironov@domclick.ru>
2022-11-21 08:28:14 +0300

    lsm data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QTextEdit, QVBoxLayout, QWidget, QComboBox
+from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QTextEdit, QVBoxLayout, QWidget, QComboBox, QCheckBox
 from PySide2.QtCore import Qt
 from random import choice
 from serial.tools import list_ports
@@ -36,9 +36,37 @@ class History(QWidget, FormHistory):
         self.setupUi(self)
 
 
-def getParams():
-    #for i in 
-    pass
+class DataСarrier:
+    def __innit__(self, dict_params):
+        self.dict_params = dict_params
+    
+    def getParam(self):
+        res={}
+        for k, v in self.dict_params.items():
+            if isinstance(v, QTextEdit):
+                res[k]=v.toPlainText()
+            elif isinstance(v, QLineEdit):
+                res[k]=v.text()
+            elif isinstance(v, QComboBox):
+                res[k]=v.currentIndex()
+            elif isinstance(v, QCheckBox):
+                    res[k]=v.isChecked()
+        return res
+    
+    def setParam(self, set_param):
+        for k, v in self.set_param.items():
+            if k in self.dict_params:
+                if isinstance(self.dict_params[k], QTextEdit):
+                    self.dict_params[k].setPlainText(v)
+                elif isinstance(v, QLineEdit):
+                    self.dict_params[k].text(v)
+                elif isinstance(v, QComboBox):
+                    self.dict_params[k].setCurrentIndex(v)
+                elif isinstance(v, QCheckBox):
+                    self.dict_params[k].setChecked(v)
+
+
+
 
 app = QApplication(sys.argv)
 
@@ -57,4 +85,10 @@ window.menu.actions()[1].triggered.connect(setting_modem.show)
 
 dict_param = {'__'.join(t.objectName().split('__')[1:]):t for t in  window.findChildren(QTextEdit) if t.objectName().find('lsm__') >= 0}
 dict_param.update({'__'.join(t.objectName().split('__')[1:]):t for t in  window.findChildren(QComboBox) if t.objectName().find('lsm__') >= 0}) 
+dict_param.update({'__'.join(t.objectName().split('__')[1:]):t for t in  setting_usb.findChildren(QTextEdit) if t.objectName().find('lsm__') >= 0}) 
+dict_param.update({'__'.join(t.objectName().split('__')[1:]):t for t in  setting_usb.findChildren(QComboBox) if t.objectName().find('lsm__') >= 0}) 
+dict_param.update({'__'.join(t.objectName().split('__')[1:]):t for t in  history.findChildren(QTextEdit) if t.objectName().find('lsm__') >= 0}) 
+dict_param.update({'__'.join(t.objectName().split('__')[1:]):t for t in  history.findChildren(QComboBox) if t.objectName().find('lsm__') >= 0}) 
+
+data_carrier = DataСarrier(dict_param)
 app.exec_()
